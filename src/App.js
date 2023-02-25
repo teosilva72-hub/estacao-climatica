50be2f93dd40a914776511e357c4f429a944c743
teosilva72 <representate.com.sp@gmail.com>
2023-02-24 22:10:05 -0300

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ReactDOM } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null,
+      station: null,
+      date: null,
+      icon: null
+    }
+  }
+
+  toObtainStation = (date, latitude) => {
+    const yearCurrent = date.getFullYear();
+
+    const dateOne = new Date(yearCurrent, 5, 23);
+    const dateTwo = new Date(yearCurrent, 8, 24);
+    const dateThree = new Date(yearCurrent, 11, 24);
+    const dateFour = new Date(yearCurrent, 2, 21);
+
+    const south = latitude < 0
+    if (date >= dateOne && date < dateTwo)
+      return south ? 'Primavera' : 'Inverno';
+    if (date >= dateTwo && date < dateThree)
+      return south ? 'Primavera' : 'Outono';
+    if (date >= dateThree && date < dateFour)
+      return south ? 'Verão' : 'inverno';
+    return south ? 'Outono' : 'Primavera';
+
+
+  }
+
+  icon = {
+    'spring': 'bi bi-flower1',
+    'summer': 'bi bi-brightness-high-fill',
+    'autumn': 'bi bi-tree-fill',
+    'winter': 'bi bi-snow2'
+  }
+  
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
 }
 
 export default App;
